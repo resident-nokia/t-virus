25855e4aefcda36bc99402201514c6f1460b7639
c79 <vexed@riseup.net>
2021-10-11 16:32:06 +0300

    Some improvements
    download.sh:
    	* Make download prettier with y/n prompt instead of same start prompt
    	* Notifies when download is skipped so users know their prompt was respected and it skipped
    
    flash_all.sh:
    	* Run check_if_dir_exists as early as possible
    	* Make prompt to download firmware nicer

--- download.sh
+++ download.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -euf
+
 if ! [ -f "$PWD/.tvirus_jitb1NGl" ]
 then
-        echo "You must run this from the root of the repo."
-        exit 1
+    echo "You must run this from the root of the repo."
+    exit 1
 fi
 
 firmware="$PWD/images"
@@ -11,70 +13,59 @@ temp="$PWD/temp"
 mkdir -p "$temp"
 trap 'rm -r "${temp:?}"' EXIT
 
-if [ -z "$_treble_download_dest" ]
+if [ -z "${_treble_download_dest:-}" ]
 then
-	echo "Welcome to T-Virus downloader!"
-	select opt in "Convert" "Revert"; do
-	    case $opt in
-	      "Convert")
-	        dest="treble"
-	        break
-	        ;;
-	      "Revert")
-	        dest="stock"
-	        break
-	        ;;
-	        *)
-	        echo "Your input should be one of the numbers in the list!"
-	    esac
-	done
+    echo "Welcome to T-Virus downloader!"
+    select opt in "Convert" "Revert"; do
+        case $opt in
+            "Convert")
+                dest="treble"
+                break
+                ;;
+            "Revert")
+                dest="stock"
+                break
+                ;;
+            *)
+                echo "Your input should be one of the numbers in the list!"
+        esac
+    done
 else
-	dest=$_treble_download_dest
+	dest="$_treble_download_dest"
 fi
 
 function already_exists() {
-    if [ -d "$1" ]
+    if [ -d "$firmware/$1" ]
     then
-        echo
-        echo "$1 already exists, want to start over?"
-        select opt in "Yes" "No"; do
-            case $opt in
-               "Yes") return 0 ;;
-               "No") return 1 ;;
-                *) echo "Your input should be one of the numbers in the list!"
+        while true
+        do
+            read -r -p "Images for $1"$' already exist.\nDo you want to start over? [y/n]: ' yn
+            case $yn in
+                [Yy]*) return 0 ;;
+                [Nn]*) return 1 ;;
             esac
+            echo
         done
     else
         return 0
     fi
+
     return 1
 }
 
+echo
 mkdir -p "$firmware"
-
-if already_exists "$firmware/common"
-then
-    wget -O "$temp/common.zip" 'https://github.com/resident-nokia/t-virus/releases/download/tvirus-images_2021-10-08/common.zip'
-    mkdir -p "$firmware/common"
-    unzip "$temp/common.zip" -d "$firmware/common"
+for image in "common" "$dest"
+do
+    echo "Downloading $image..."
+    if already_exists "$image"
+    then
+        wget --no-verbose --show-progress -O "$temp/$image.zip" "https://github.com/resident-nokia/t-virus/releases/download/tvirus-images_2021-10-08/$image.zip"
+        mkdir -p "$firmware/$image"
+        unzip "$temp/$image.zip" -d "$firmware/$image"
+        echo "Download for $image is completed."
+    else
+        echo "Download for $image is skipped."
+    fi
     echo
-fi
-
-if [ "$dest" == "treble" ]
-then
-    if already_exists "$firmware/treble"
-    then
-        wget -O "$temp/treble.zip" 'https://github.com/resident-nokia/t-virus/releases/download/tvirus-images_2021-10-08/treble.zip'
-        mkdir -p "$firmware/treble"
-        unzip "$temp/treble.zip" -d "$firmware/treble"
-        echo
-    fi
-else
-    if already_exists "$firmware/stock"
-    then
-        wget -O "$temp/stock.zip" 'https://github.com/resident-nokia/t-virus/releases/download/tvirus-images_2021-10-08/stock.zip'
-        mkdir -p "$firmware/stock"
-        unzip "$temp/stock.zip" -d "$firmware/stock"
-        echo
-    fi
-fi
+done
